6b361b01d71fde28184e411d726d2c962b2d17b5
samgozman <sam@gozman.space>
2022-05-04 22:57:32 +0400

    Fixed typo

--- internal/utils/fcf.go
+++ internal/utils/fcf.go
@@ -7,7 +7,7 @@ type FCFParams struct {
 	NOPAT         float32 // Net Operating Profit After Tax
 }
 
-// Caclulate Unlevered FCF
+// Calculate Unlevered FCF
 func UnleveredFCF(params FCFParams) float32 {
 	return params.NOPAT + float32(params.NWCInvestment) + float32(params.DA) - float32(params.CapEx)
 }
