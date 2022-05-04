c2b80aac18c1694cf328aead15a1ecade0ff3a41
samgozman <sam@gozman.space>
2022-05-05 00:32:26 +0400

    Add PerpetuityGrowthPeriod DFC method and test for it

--- dcf/perpetuity-growth.go
+++ dcf/perpetuity-growth.go
@@ -1,18 +1,98 @@
 package dcf
 
-// TODO: Rename, refactor
+import (
+	"errors"
+	"time"
+
+	"github.com/samgozman/valuation/internal/utils"
+)
+
+// First index is always the current state of balance sheet.
+// If you want to calculate DCF for 5 years when you need to
+// pass 6 elements of PerpetuityGrowthPeriod into function.
+// First element will be the current state state of balance sheet
+// and the other 5 will represent your assumptions.
 type PerpetuityGrowthPeriod struct {
-	Revenue      int64
-	EBITDA       int64   // Earnings before interest, taxes, depreciation, and amortization
-	CapEx        int64   // Capital Expenditures
-	NWC          int64   // Net Working Capital
-	DA           int64   // Depreciation & Amortization
-	TaxRate      float32 // Tax rate assumption
-	DiscountRate float32 // Discount rate (interest rate). Usually equals to WACC
-	PGR          float32 // Perpetuity Growth Rate - Typically between the inflation rate of 2-3% and the GDP growth rate of 4-5%
+	Revenue       int64
+	EBITDA        int64     // Earnings before interest, taxes, depreciation and amortization
+	CapEx         int64     // Capital Expenditures
+	NWC           int64     // Net Working Capital
+	DA            int64     // Depreciation & Amortization
+	TaxRate       float32   // Tax rate assumption
+	DiscountRate  float32   // Discount rate (interest rate). Usually equals to WACC
+	PGR           float32   // Perpetuity Growth Rate - Typically between the inflation rate of 2-3% and the GDP growth rate of 4-5%
+	BeginningDate time.Time // Balance Sheet beginning date
+	EndingDate    time.Time // Balance Sheet ending date
 }
 
-// Perpetuity Growth Model
-func PerpetuityGrowthFairValue(periods *[]PerpetuityGrowthPeriod) {
+// Calculate enterprise value by Perpetuity Growth Model
+func PerpetuityGrowth(periods *[]PerpetuityGrowthPeriod) (float32, error) {
+	periodsNumber := len(*periods)
 
+	if periodsNumber < 2 {
+		return 0, errors.New("number of periods must be 2+")
+	}
+
+	// 1. Calculate discrete Cash Flows
+	var dFCF_sum float32
+	var terminal_fcf float32
+	for i := 1; i < periodsNumber; i++ {
+		nopat := utils.NOPAT(utils.NOPATParams{
+			EBITDA:  (*periods)[i].EBITDA,
+			DA:      (*periods)[i].DA,
+			TaxRate: (*periods)[i].TaxRate,
+		})
+
+		nwcInvestment := utils.NWCInvestment((*periods)[i-1].NWC, (*periods)[i].NWC)
+
+		fcf := utils.UnleveredFCF(utils.FCFParams{
+			NWCInvestment: nwcInvestment,
+			CapEx:         (*periods)[i].CapEx,
+			DA:            (*periods)[i].DA,
+			NOPAT:         nopat,
+		})
+
+		dp := utils.DiscountingPeriod(utils.BalanceSheetDates{
+			Today: time.Now(),
+			Begin: (*periods)[i].BeginningDate,
+			End:   (*periods)[i].EndingDate,
+		})
+
+		df := utils.DiscountFactor(utils.DiscountFactorParams{
+			PeriodsNumber: dp,
+			DiscountRate:  (*periods)[i].DiscountRate,
+		})
+
+		dFCF_sum += fcf * df
+		terminal_fcf = fcf
+	}
+
+	// 2. Calculate terminal discounting periods
+	discountingPeriod := utils.DiscountingPeriod(utils.BalanceSheetDates{
+		Today: time.Now(),
+		Begin: (*periods)[periodsNumber-1].BeginningDate,
+		End:   (*periods)[periodsNumber-1].EndingDate.Add(365 / 2 * 24 * time.Hour),
+	})
+
+	// 3. Calculate terminal discount factor
+	discountFactor := utils.DiscountFactor(utils.DiscountFactorParams{
+		PeriodsNumber: discountingPeriod,
+		DiscountRate:  (*periods)[periodsNumber-1].DiscountRate,
+	})
+
+	// 4. Calculate terminal value
+	terminalValue := utils.TerminalValue(utils.TerminalValueParams{
+		FCF:          terminal_fcf,
+		DiscountRate: (*periods)[periodsNumber-1].DiscountRate,
+		PGR:          (*periods)[periodsNumber-1].PGR,
+	})
+
+	// 5. Calculate enterprise value
+	enterpriseValue := utils.EnterpriseValueFromFCF(utils.EVFromFCFParams{
+		TV:                terminalValue,
+		DiscreteCashFlows: dFCF_sum,
+		DiscountFactor:    discountFactor,
+	})
+
+	return enterpriseValue, nil
 }
---
+++ dcf/perpetuity-growth_test.go
@@ -0,0 +1,92 @@
+package dcf
+
+import (
+	"testing"
+	"time"
+)
+
+func TestPerpetuityGrowth(t *testing.T) {
+	t.Run("Should calculate EV by PerpetuityGrowth", func(t *testing.T) {
+		periods := []PerpetuityGrowthPeriod{
+			{
+				Revenue:       365817,
+				EBITDA:        120233,
+				CapEx:         11085,
+				NWC:           9975,
+				DA:            11284,
+				TaxRate:       0.17,
+				DiscountRate:  0.083,
+				PGR:           0.025,
+				BeginningDate: time.Now(),
+				EndingDate:    time.Now(),
+			},
+			{
+				Revenue:       392625,
+				EBITDA:        131478,
+				CapEx:         11409,
+				NWC:           2929,
+				DA:            12076,
+				TaxRate:       0.17,
+				DiscountRate:  0.083,
+				PGR:           0.025,
+				BeginningDate: time.Date(2022, 3, 26, 0, 0, 0, 0, time.UTC),
+				EndingDate:    time.Date(2022, 9, 30, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				Revenue:       416632,
+				EBITDA:        134219,
+				CapEx:         12904,
+				NWC:           2623,
+				DA:            10281,
+				TaxRate:       0.17,
+				DiscountRate:  0.083,
+				PGR:           0.025,
+				BeginningDate: time.Date(2022, 9, 30, 0, 0, 0, 0, time.UTC),
+				EndingDate:    time.Date(2023, 9, 30, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				Revenue:       430880,
+				EBITDA:        139974,
+				CapEx:         14168,
+				NWC:           1557,
+				DA:            14459,
+				TaxRate:       0.17,
+				DiscountRate:  0.083,
+				PGR:           0.025,
+				BeginningDate: time.Date(2023, 9, 30, 0, 0, 0, 0, time.UTC),
+				EndingDate:    time.Date(2024, 9, 30, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				Revenue:       457900,
+				EBITDA:        157853,
+				CapEx:         17428,
+				NWC:           2952,
+				DA:            15037,
+				TaxRate:       0.17,
+				DiscountRate:  0.083,
+				PGR:           0.025,
+				BeginningDate: time.Date(2024, 9, 30, 0, 0, 0, 0, time.UTC),
+				EndingDate:    time.Date(2025, 9, 30, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				Revenue:       518374,
+				EBITDA:        186210,
+				CapEx:         16386,
+				NWC:           6607,
+				DA:            13027,
+				TaxRate:       0.17,
+				DiscountRate:  0.083,
+				PGR:           0.025,
+				BeginningDate: time.Date(2025, 9, 30, 0, 0, 0, 0, time.UTC),
+				EndingDate:    time.Date(2026, 9, 30, 0, 0, 0, 0, time.UTC),
+			},
+		}
+
+		want := 2206649
+		got, _ := PerpetuityGrowth(&periods)
+
+		if want != int(got) {
+			t.Errorf("Expected '%v', but got '%v'", want, got)
+		}
+	})
+}
