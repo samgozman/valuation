1985563b510e1fb8c6db52df3f7e5f45f67d7abd
samgozman <sam@gozman.space>
2022-05-08 10:15:29 +0400

    Add new DCF method: EBITDA exit

---
+++ dcf/ebita-exit_test.go
@@ -0,0 +1,97 @@
+package dcf
+
+import (
+	"testing"
+	"time"
+)
+
+func TestEBITDAExit(t *testing.T) {
+	t.Run("Should calculate EV by EBITDA Exit strategy", func(t *testing.T) {
+		periods := []EBITDAExitPeriod{
+			{
+				NWC: 9975,
+			},
+			{
+				Revenue:       392625,
+				EBITDA:        131478,
+				OtherIncome:   0,
+				CapEx:         11409,
+				NWC:           2929,
+				DA:            12076,
+				TaxRate:       0.17,
+				DiscountRate:  0.083,
+				BeginningDate: time.Date(2022, 3, 26, 0, 0, 0, 0, time.UTC),
+				EndingDate:    time.Date(2022, 9, 30, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				Revenue:       416632,
+				EBITDA:        134219,
+				OtherIncome:   0,
+				CapEx:         12904,
+				NWC:           2623,
+				DA:            10281,
+				TaxRate:       0.17,
+				DiscountRate:  0.083,
+				BeginningDate: time.Date(2022, 9, 30, 0, 0, 0, 0, time.UTC),
+				EndingDate:    time.Date(2023, 9, 30, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				Revenue:       430880,
+				EBITDA:        139974,
+				OtherIncome:   0,
+				CapEx:         14168,
+				NWC:           1557,
+				DA:            14459,
+				TaxRate:       0.17,
+				DiscountRate:  0.083,
+				BeginningDate: time.Date(2023, 9, 30, 0, 0, 0, 0, time.UTC),
+				EndingDate:    time.Date(2024, 9, 30, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				Revenue:       457900,
+				EBITDA:        157853,
+				OtherIncome:   0,
+				CapEx:         17428,
+				NWC:           2952,
+				DA:            15037,
+				TaxRate:       0.17,
+				DiscountRate:  0.083,
+				BeginningDate: time.Date(2024, 9, 30, 0, 0, 0, 0, time.UTC),
+				EndingDate:    time.Date(2025, 9, 30, 0, 0, 0, 0, time.UTC),
+			},
+			{
+				Revenue:       518374,
+				EBITDA:        186210,
+				OtherIncome:   0,
+				CapEx:         16386,
+				NWC:           6607,
+				DA:            13027,
+				TaxRate:       0.17,
+				DiscountRate:  0.083,
+				BeginningDate: time.Date(2025, 9, 30, 0, 0, 0, 0, time.UTC),
+				EndingDate:    time.Date(2026, 9, 30, 0, 0, 0, 0, time.UTC),
+			},
+		}
+		currentDate := time.Date(2022, 5, 4, 0, 0, 0, 0, time.UTC)
+		EBITDAExitMultiple := float32(15.1)
+
+		want := 2449491
+
+		got, _ := EBITDAExit(&periods, currentDate, EBITDAExitMultiple)
+
+		if want != int(got) {
+			t.Errorf("Expected '%v', but got '%v'", want, got)
+		}
+	})
+
+	t.Run("Should return error if periods number are less than 2", func(t *testing.T) {
+		periods := []PerpetuityGrowthPeriod{}
+		currentDate := time.Date(2022, 5, 4, 0, 0, 0, 0, time.UTC)
+
+		_, err := PerpetuityGrowth(&periods, currentDate)
+
+		if err == nil {
+			t.Error("Expected error but didn't get it")
+		}
+	})
+}
---
+++ dcf/ebitda-exit.go
@@ -0,0 +1,97 @@
+package dcf
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/samgozman/valuation/internal/utils"
+)
+
+// First index is always the current state of balance sheet.
+// If you want to calculate DCF for 5 years when you need to
+// pass 6 elements of EBITDAExitPeriod into function.
+// First element will be the current state of balance sheet
+// and the other 5 will represent your assumptions.
+type EBITDAExitPeriod struct {
+	Revenue       int64
+	EBITDA        int64     // Earnings before interest, taxes, depreciation and amortization
+	OtherIncome   int64     // Other Income / Expenses
+	CapEx         int64     // Capital Expenditures
+	NWC           int64     // Net Working Capital
+	DA            int64     // Depreciation & Amortization
+	TaxRate       float32   // Tax rate assumption
+	DiscountRate  float32   // Discount rate (interest rate). Usually equals to WACC
+	BeginningDate time.Time // Balance Sheet beginning date
+	EndingDate    time.Time // Balance Sheet ending date
+}
+
+func EBITDAExit(periods *[]EBITDAExitPeriod, currentDate time.Time, EBITDAExitMultiple float32) (float32, error) {
+	periodsNumber := len(*periods)
+
+	if periodsNumber < 2 {
+		return 0, errors.New("number of periods must be 2+")
+	}
+
+	// 1. Calculate discrete Cash Flows
+	var dFCF_sum float32
+	for i := 1; i < periodsNumber; i++ {
+		nopat := utils.NOPAT(utils.NOPATParams{
+			EBITDA:      (*periods)[i].EBITDA,
+			OtherIncome: (*periods)[i].OtherIncome,
+			DA:          (*periods)[i].DA,
+			TaxRate:     (*periods)[i].TaxRate,
+		})
+
+		nwcInvestment := utils.NWCInvestment((*periods)[i-1].NWC, (*periods)[i].NWC)
+
+		fcf := utils.UnleveredFCF(utils.FCFParams{
+			NWCInvestment: nwcInvestment,
+			CapEx:         (*periods)[i].CapEx,
+			DA:            (*periods)[i].DA,
+			NOPAT:         nopat,
+		})
+
+		dp := utils.DiscountingPeriod(utils.BalanceSheetDates{
+			Today: currentDate,
+			Begin: (*periods)[i].BeginningDate,
+			End:   (*periods)[i].EndingDate,
+		})
+
+		df := utils.DiscountFactor(utils.DiscountFactorParams{
+			PeriodsNumber: dp,
+			DiscountRate:  (*periods)[i].DiscountRate,
+		})
+
+		dFCF_sum += fcf * df
+	}
+
+	// 2. Calculate terminal discounting periods
+	discountingPeriod := utils.DiscountingPeriod(utils.BalanceSheetDates{
+		Today: currentDate,
+		Begin: (*periods)[periodsNumber-1].BeginningDate,
+		End:   (*periods)[periodsNumber-1].EndingDate,
+	}) + 0.5
+
+	// 3. Calculate terminal discount factor
+	discountFactor := utils.DiscountFactor(utils.DiscountFactorParams{
+		PeriodsNumber: discountingPeriod,
+		DiscountRate:  (*periods)[periodsNumber-1].DiscountRate,
+	})
+
+	fmt.Println("EBITDAExitMultiple", EBITDAExitMultiple, "TerminalEBITDA", (*periods)[periodsNumber-1].EBITDA, "DiscountFactor", discountFactor)
+
+	// 4. Calculate terminal value
+	terminalValue := utils.TerminalValueEBITDAExit(utils.TerminalValueEBITDAParams{
+		EBITDAExitMultiple: EBITDAExitMultiple,
+		TerminalEBITDA:     float32((*periods)[periodsNumber-1].EBITDA),
+		DiscountFactor:     discountFactor,
+	})
+
+	fmt.Println("terminalValue", terminalValue, "dFCF_sum", dFCF_sum)
+
+	// 5. Calculate enterprise value
+	enterpriseValue := terminalValue + dFCF_sum
+
+	return enterpriseValue, nil
+}
